6cd32f49907df507912d060e8f76ee1beb73acb3
Mike Morcerf <mikemorcerf@gmail.com>
2020-10-20 18:54:51 -0400

    create Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
-          <img src="" alt=""/>
+          <img src={logoImg} alt="Proffy"/>
+          <h2>Your online study platform</h2>
         </div>
+
+        <img
+          src={landingImg}
+          alt="Study platform"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Study"/>
+            Study
+          </a>
+
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="Teach"/>
+            Teach
+          </a>
+        </div>
+
+        <span className="total-connections">
+          200 connections established <img src={purpleHeartIcon} alt="Purple heart"/>
+        </span>
       </div>
     </div>
   )
