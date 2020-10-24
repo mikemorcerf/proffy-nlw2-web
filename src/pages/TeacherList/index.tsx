2db70659dda379f5913c6925805672a3804432e5
Mike Morcerf <mikemorcerf@gmail.com>
2020-10-24 17:51:23 -0400

    style TeacherList page

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -7,7 +7,24 @@ import './styles.css';
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Here are all available proffys:"/>
+      <PageHeader title="Here are all available proffys:">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Subject</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Day of the week</label>
+            <input type="text" id="week-day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Time</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
     </div>
   )
 }
